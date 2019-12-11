880506134b19720362ec6af06c731268e087ce4d
leonardo_vernaza <leonardo.vernaza@admobilize.com>
2019-12-11 14:56:56 -0500

    Add checksum

--- ww_icl_redist/tools/chocolateyinstall.ps1
+++ ww_icl_redist/tools/chocolateyinstall.ps1
@@ -4,6 +4,8 @@ $tempPath = [System.IO.Path]::GetFullPath("${Env:TEMP}\intelRedistributable")
 
 $fileUrl    = "$tempPath\ww_icl_redist_msi\ww_icl_redist_ia32_2019.5.281.msi"
 $fileUrl64  = "$tempPath\ww_icl_redist_msi\ww_icl_redist_intel64_2019.5.281.msi"
+$checksum   = "9ec6f0594556eacdecc7f06bbe5471bd72c4d4a64d78731ad2a9e6faa360efe5"
+$checksum64 = "96396b4225836a17e482abefbd0d575e56cb6329bd0302357aed4c97d04e17cd"
 
 $url        = 'http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/15799/ww_icl_redist_msi_2019.5.281.zip'
 
@@ -34,4 +36,6 @@ Install-ChocolateyInstallPackage `
   -SilentArgs '/quiet' `
   -File64 $fileUrl64 `
   -File $fileUrl `
+  -Checksum64 $checksum `
+  -Checksum $checksum `
   -ValidExitCodes @(0, 3010)
